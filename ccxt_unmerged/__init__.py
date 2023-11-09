6afb7f9c808c54a280dba76e2a823f0ceda21a5b
binares <binares@protonmail.com>
2023-11-09 13:52:23 +0200

    [remove] half of the exchanges
    added to ccxt:
     - bitopro
     - bitrue
     - cryptocom
     - wazirx
    
    out of service:
     - _58coin
     - bcio
     - beaxy
     - biki
     - bkex
     - ceo
     - coinbene [delist from uxs as well!!]
     - coinsuper
     - cossdex
     - dragonex
     - duedex
     - multi
     - mxc
     - silgonex
     - tokensnet
     - txbit
     - vinex

--- ccxt_unmerged/__init__.py
+++ ccxt_unmerged/__init__.py
@@ -1,57 +1,36 @@
 """
 Exchanges that are not present in ccxt
 """
-__version__ = '2.2'
-__author__ = 'binares'
+__version__ = "4.0"
+__author__ = "binares"
 
 import ccxt
 import sys
 import warnings
 
-from ._58coin import _58coin
-from .bcio import bcio
-from .beaxy import beaxy
-from .biki import biki
 from .bitclude import bitclude
 from .bitforexfu import bitforexfu
 from .bitkub import bitkub
-from .bitopro import bitopro
-from .bitrue import bitrue
-from .bkex import bkex
 from .btse import btse
-from .ceo import ceo
 from .changellypro import changellypro
-from .coinbene import coinbene
 from .coindcx import coindcx
-from .coinsuper import coinsuper
-from .cossdex import cossdex
-from .cryptocom import cryptocom
-from .dragonex import dragonex
-from .duedex import duedex
 from .felixo import felixo
 from .foblgate import foblgate
 from .gateiofu import gateiofu
 from .krakenfu import krakenfu
-from .multi import multi
-from .mxc import mxc
 from .nominex import nominex
 from .primexbt import primexbt
 from .remitano import remitano
-from .silgonex import silgonex
 from .slicex import slicex
 from .tokenomy import tokenomy
-from .tokensnet import tokensnet
 from .tradeogre import tradeogre
-from .txbit import txbit
-from .vinex import vinex
 from .vitex import vitex
-from .wazirx import wazirx
 from .yunex import yunex
 
 _ALREADY_MERGED = []
 
 # Add the custom-defined exchanges to ccxt
-for attr,value in list(globals().items()):
+for attr, value in list(globals().items()):
     if isinstance(value, type) and issubclass(value, ccxt.Exchange):
         if not hasattr(ccxt, attr):
             setattr(ccxt, attr, value)
@@ -59,7 +38,7 @@ for attr,value in list(globals().items()):
             _ALREADY_MERGED.append(attr)
 
 if sys.version_info >= (3, 5, 3):
-    from . import async_support # initialize async exchanges
+    from . import async_support  # initialize async exchanges
 
 
 def warn_duplicated():
@@ -67,6 +46,10 @@ def warn_duplicated():
     might differ from the one previously defined here"""
     if _ALREADY_MERGED:
         merged_exchanges = "'" + "', '".join(_ALREADY_MERGED) + "'"
-        warnings.warn("Exchanges %s have been added to ccxt. If you were using the old version (in ccxt_unmerged), there might be some differences." % merged_exchanges)
+        warnings.warn(
+            "Exchanges %s have been added to ccxt. If you were using the old version (in ccxt_unmerged), there might be some differences."
+            % merged_exchanges
+        )
+
 
 warn_duplicated()
--- setup.py
+++ setup.py
@@ -8,16 +8,18 @@ README = (HERE / "README.md").read_text()
 is_python_2 = sys.version_info < (3, 0)
 
 setup(
-   name='ccxt-unmerged',
-   version='2.2',
-   description="Some 40 exchanges that haven't been merged into ccxt yet.",
-   long_description=README,
-   long_description_content_type='text/markdown',
-   url='https://github.com/binares/ccxt-unmerged',
-   author='binares',
-   author_email='binares@protonmail.com',
-   packages=find_packages(exclude=['ccxt_unmerged.async_support*'] if is_python_2 else []),
-   install_requires=[
-       'ccxt',
-   ],
+    name="ccxt-unmerged",
+    version="4.0",
+    description="Some 40 exchanges that haven't been merged into ccxt yet.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/binares/ccxt-unmerged",
+    author="binares",
+    author_email="binares@protonmail.com",
+    packages=find_packages(
+        exclude=["ccxt_unmerged.async_support*"] if is_python_2 else []
+    ),
+    install_requires=[
+        "ccxt",
+    ],
 )
