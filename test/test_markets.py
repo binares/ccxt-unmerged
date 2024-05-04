c3c6a5735350a1b5439e37a99d74ac93206670c2
binares <binares@protonmail.com>
2024-05-04 11:40:48 +0300

    test_markets.py

---
+++ test/test_markets.py
@@ -0,0 +1,27 @@
+# import pytest
+import ccxt_unmerged
+import ccxt
+import traceback
+
+
+def get_exchange_classes():
+    exchange_classes = []
+    for name in dir(ccxt_unmerged):
+        obj = getattr(ccxt_unmerged, name)
+        if isinstance(obj, type) and issubclass(obj, ccxt.Exchange):
+            exchange_classes.append(obj)
+    return exchange_classes
+
+
+def get_exchanges():
+    return [xc() for xc in get_exchange_classes()]
+
+
+def test_load_markets():
+    for xc_cls in get_exchange_classes():
+        print("Loading {}'s markets".format(xc_cls.__name__))
+        try:
+            xc = xc_cls()
+            xc.load_markets()
+        except Exception as e:
+            traceback.print_exc()
